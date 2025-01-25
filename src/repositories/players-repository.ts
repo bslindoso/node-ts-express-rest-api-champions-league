2f19efc1f1e47e4ca6455aca25550054a2d41485
Bruno Lindoso <48108317+bslindoso@users.noreply.github.com>
2025-01-25 09:06:38 -0300

    list all players done

--- src/repositories/players-repository.ts
+++ src/repositories/players-repository.ts
@@ -1,15 +1,12 @@
+import fs from 'fs'
+import path from 'path'
 import { PlayerModel } from "../models/player-model"
 
-const database: PlayerModel[] = [
-  {
-    id: 1,
-    name: "CR7"
-  },
-  {
-    id: 2,
-    name: "Messi"
-  }
-]
+const dataFile = path.join(__dirname, '..', 'data', 'players.json')
+const language = 'utf-8'
+
+
+const database: PlayerModel[] = JSON.parse(fs.readFileSync(dataFile, language))
 
 export const findAllPlayers = async (): Promise<PlayerModel[]> => {
   return database
