458b4b1773b21a1c6672312ef350ca2ed4b32594
Bruno Lindoso <48108317+bslindoso@users.noreply.github.com>
2025-01-25 00:40:58 -0300

    project init

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+﻿import express, { json, Request, Response } from 'express'
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(json())
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(400).json({
+    player: "Ronaldinho"
+  })
+})
+
+app.listen(port, () => {
+  console.log(`🔥 Server running at port ${port}`)
+})
