f3f38c1c2a3a3e334b93b7c6ab03ad34cfb15357
Bruno Lindoso <48108317+bslindoso@users.noreply.github.com>
2025-01-27 18:09:44 -0300

    installing cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import express from 'express'
 import router from './routes';
+import cors from 'cors'
 
 export default function createApp() {
   const app = express();
 
   app.use(express.json());
-
   app.use("/api", router)
+
+  // EXEMPLO DE USO DE CORS
+  // const corsOptions = {
+  //   origin: ['http://brunolindoso.system.com','http://teste.gov.br'],
+  //   methods: ['GET']
+  // }
+  // app.use(cors(corsOptions))
+
+  app.use(cors())
 
   return app;
 }
